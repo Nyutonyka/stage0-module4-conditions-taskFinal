119ba4de07c9184476b34498b9b128e61cf1a6d3
nyuto <nuytonyka@gmail.com>
2023-02-03 21:42:10 +0200

    Solved

--- src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
@@ -2,6 +2,18 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class DaysInMonth {
     public void printDays(int year, int month) {
+        int count;
+        if((year%4 == 0 && year% 100 != 0)|| year%400 == 0){
+            count = 1;
+        }else{
+            count = 0;
+        }
 
+        switch (month){
+            case 1,3,5,7,9,11 -> System.out.println(31);
+            case 2 -> System.out.println(28 + count);
+            case 4,6,8,10,12 -> System.out.println(30);
+            default -> System.out.println("invalid date");
+        }
     }
 }
